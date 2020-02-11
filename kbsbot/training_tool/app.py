2fa32f7a427013213352987f7ac7092e7ac36272
Andre <andreherrera97@hotmail.com>
2020-02-11 02:36:31 -0500

    Adding cors

--- kbsbot/training_tool/app.py
+++ kbsbot/training_tool/app.py
@@ -1,6 +1,7 @@
 import os
 from flakon import create_app as _create_app
 from .views import blueprints
+from flask_cors import CORS
 
 _HERE = os.path.dirname(__file__)
 _SETTINGS = os.path.join(_HERE, 'settings.ini')
@@ -11,4 +12,5 @@ def create_app(settings=None):
         settings = _SETTINGS
 
     app = _create_app(blueprints=blueprints, settings=settings)
+    app = CORS(app)
     return app
